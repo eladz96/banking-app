24cc2a1694ab40945e149a24d41e029813c9d7e8
elad.zaltsman <eladz96@gmail.com>
2025-04-07 10:17:00 +0300

    changes to back for adding name to user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,7 @@ exports.registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await User.create({
+            name,
             email,
             password: hashedPassword,
             phone,
@@ -33,7 +34,7 @@ exports.loginUser = async (req, res) => {
             return res.status(401).json({ message: "Invalid credentials" });
         }
 
-        const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ id: user._id, email: user.email , name: user.name}, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.json({ token });
     } catch (err) {
         res.status(500).json({ message: "Login failed", error: err.message });
